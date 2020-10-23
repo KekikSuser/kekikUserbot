8db3f7b60830f41456dd5185b17fcbd324f38da4
Ömer Faruk Sancak <keyiflerolsun@gmail.com>
2020-10-23 23:27:02 +0300

    .env hata yönetimi için Heroku Geçme..

--- Userbot/__init__.py
+++ Userbot/__init__.py
@@ -1,6 +1,7 @@
 # Bu araç @keyiflerolsun tarafından | @KekikAkademi için yazılmıştır.
 
 from pyrogram import Client, __version__
+from konsolTaban._evrensel.tanimlar import bellenim_surumu
 import os, sys
 from dotenv import load_dotenv
 from rich.console import Console
@@ -25,7 +26,7 @@ if sys.version_info[0] < 3 or sys.version_info[1] < 6:
               Birden fazla özellik buna bağlıdır. Bot kapatılıyor.""")
     quit(1)
 
-if not os.path.exists('ayar.env'):
+if (bellenim_surumu.split('-')[-1] != 'aws') and (not os.path.exists("ayar.env")): # Heroku Geçmek için aws
     hata("\n\tLütfen ayar.env dosyanızı oluşturun..\n")
     quit(1)
 
